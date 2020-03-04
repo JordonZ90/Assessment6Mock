409504e53f8a50b61514b8771d0278292fb1e7a5
Jordon <jzenterprize90@gmail.com>
2020-03-04 08:49:11 -0500

    Mock Test 6

---
+++ Assessment6Mock/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Assessment6Mock.Models;
+
+namespace Assessment6Mock.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly EmployeeDbContext _context; // NEED this
+
+        public HomeController(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Employee()
+        {
+            return View(_context.Employee.ToList());
+        }
+        public IActionResult RetirementInfo(int Id)
+        {
+            Employee findEmployee = _context.Employee.Find(Id);
+            if (findEmployee.Age > 60)
+            {
+                ViewBag.CanRetire = true;
+            }
+            else
+            {
+                ViewBag.CanRetire = false;
+                //return RedirectToAction("SomeOtherAction");
+            }
+            ViewBag.Benefits = findEmployee.Salary * 0.6; //(double)findEmployee.Salary * 0.6;
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
